a3dab78532af0402b943ee254f4c9de43eb0622e
= <allison.butler1414@gmail.com>
2017-05-08 16:31:40 -0400

    fixed data issue

--- ee629_final.py
+++ ee629_final.py
@@ -5,8 +5,12 @@ import matplotlib
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 
 data = p.read_csv('rpidata.csv')
+
+for line in data:
+    if data['Date/Time'].empty: print "uh oh"
 
 t = data['Date/Time']
 x = data['CPU Usage %']
@@ -16,17 +20,30 @@ t0 = np.array(t)
 x0 = np.array(x)
 y0 = np.array(y)
 
+#histogram settings
+num_bins = 50
+
+#Create cpu histogram
+print 'generating cpu usage histogram'
+n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='green', alpha=0.5)
+plt.xlabel('CPU Usage')
+plt.ylabel('Samples')
+plt.title('CPU Usage Histogram')
+#plt.plot(bins, x0, 'r--')
+print 'saving figure...'
+plt.savefig('usehist.jpg')
+print 'done with useage histogram!'
+plt.clf
 
 #Create the scatter plot:
 print 'generating scatter plot...'
-scatter = plt
 slope, intercept, r_value, p_value, std_err = stats.linregress(x0,y0)
 l = [slope*i + intercept for i in x0]
-scatter.xlabel('CPU Usage (%)')
-scatter.ylabel('Temperature (C)')
-scatter.title('Scatter Plot')
-scatter.plot(x0,y0, linestyle='None', marker='o', color='b')
-scatter.plot(x0, l, 'r-')
+plt.xlabel('CPU Usage (%)')
+plt.ylabel('Temperature (C)')
+plt.title('Scatter Plot')
+plt.plot(x0,y0, linestyle='None', marker='o', color='b')
+plt.plot(x0, l, 'r-')
 print 'saving figure...'
-scatter.savefig('scatter1.jpg')
+plt.savefig('scatter1.jpg')
 print 'done with scatter plot!'

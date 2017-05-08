6edabd6f134e4e7e2948114bbe6de316afd58a8f
= <allison.butler1414@gmail.com>
2017-05-08 16:39:40 -0400

    added more charts

--- ee629_final.py
+++ ee629_final.py
@@ -24,16 +24,42 @@ y0 = np.array(y)
 num_bins = 50
 
 #Create cpu histogram
-print 'generating cpu usage histogram'
+print 'generating cpu usage histogram...'
 n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='green', alpha=0.5)
 plt.xlabel('CPU Usage')
 plt.ylabel('Samples')
 plt.title('CPU Usage Histogram')
-#plt.plot(bins, x0, 'r--')
 print 'saving figure...'
 plt.savefig('usehist.jpg')
 print 'done with useage histogram!'
-plt.clf
+plt.clf()
+
+#Create temp histogram
+print 'generating cpu temp histogram...'
+n, bins, patches = plt.hist(y, num_bins, normed=1, facecolor='green', alpha=0.5)
+plt.xlabel('CPU Temp')
+plt.ylabel('Samples')
+plt.title('CPU Temp Histogram')
+print 'saving figure...'
+plt.savefig('temphist.jpg')
+print 'done with temp histogram!'
+plt.clf()
+
+#Create cpu boxplot
+print 'generating cpu usage boxplot...'
+plt.boxplot(x0)
+print 'saving figure...'
+plt.savefig('usebox.jpg')
+print 'done with usage boxplot!'
+plt.clf()
+
+#Create temp boxplot
+print 'generating cpu temp boxplot...'
+plt.boxplot(y0)
+print 'saving figure...'
+plt.savefig('tempbox.jpg')
+print 'done with temp boxplot!'
+plt.clf()
 
 #Create the scatter plot:
 print 'generating scatter plot...'
@@ -45,5 +71,5 @@ plt.title('Scatter Plot')
 plt.plot(x0,y0, linestyle='None', marker='o', color='b')
 plt.plot(x0, l, 'r-')
 print 'saving figure...'
-plt.savefig('scatter1.jpg')
+plt.savefig('scatter.jpg')
 print 'done with scatter plot!'
